7b59296e22f9950931941f6b88c4b3f7007f14e0
Dmitriy <dimaadynets@mail.ru>
2023-04-02 22:54:23 +0300

    secondCommit

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function showClock () {
     document.body.append(date);
     document.body.append(time);
     let day = clock.getDate() < 10 ? `0${clock.getDate()}` : clock.getDate();        //2 число
-    let month = clock.getMonth() < 10 ? `0${clock.getMonth()}` : clock.getMonth();     // 3  месяц (март) 0-январь 11-декабрь
+    let month = (clock.getMonth() + 1) < 10 ? `0${(clock.getMonth() + 1)}` : (clock.getMonth() + 1);     // 3  месяц (март) 0-январь 11-декабрь
     let year = clock.getFullYear();   //2023 год
     let hour = clock.getHours() < 10 ? `0${clock.getHours()}` : clock.getHours();     //17  часов
     let minute = clock.getMinutes() < 10 ? `0${clock.getMinutes()}` : clock.getMinutes();  //48 минут
